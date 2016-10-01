078e29f150dc0367d0b2dbd785def633de8d21ed
Mark Wahome <wahome@mesozi.com>
2016-10-01 11:07:13 +0300

    Update mytrips function

--- models/Trips.php
+++ models/Trips.php
@@ -132,47 +132,38 @@
 		}
 
 		function get_mytrips($trip_creator){
-
-
-			$res = query("SELECT tbl_trips.id,`start_coordinate`,`start_location`,`end_coordinate`,`end_location`,`trip_date`,`trip_time`,`date`,`vehicle_id`,`start_time`,`stop_time`,`trip_creator`,`fName`, `lName`,`phone_no`,`start_coordinate`,`start_location`,`end_coordinate`,`end_location`,`project_id`,`status`,`approval`,`fare_estimate`,`actual_fare` FROM `tbl_trips`
+			/*$res = query("SELECT tbl_trips.id,`start_coordinate`,`start_location`,`end_coordinate`,`end_location`,`trip_date`,`trip_time`,`date`,`vehicle_id`,`start_time`,`stop_time`,`trip_creator`,`fName`, `lName`,`phone_no`,`start_coordinate`,`start_location`,`end_coordinate`,`end_location`,`project_id`,`status`,`approval`,`fare_estimate`,`actual_fare` FROM `tbl_trips`
 					INNER JOIN `tbl_people` ON tbl_people.user_id = tbl_trips.trip_creator
 					LEFT JOIN `tbl_trip_approval_status` ON tbl_trip_approval_status.id = tbl_trips.approval 
 				
-				 	WHERE  tbl_trips.trip_creator= ?",$trip_creator);
-
-
+				 	WHERE  tbl_trips.trip_creator= ?",$trip_creator);*/
+			$res = query("SELECT * FROM `tbl_trips` WHERE trip_creator = ?",$trip_creator);
+			foreach ($res as $key => $trip) {				
 				$res1 = query("SELECT `fName`, `lName`,`phone_no`,`vehicle_id`,`plate`, `make`,`model` FROM `tbl_allocation`
 				INNER JOIN `tbl_people` ON tbl_allocation.driver_id = tbl_people.user_id
 				INNER JOIN `tbl_vehicles` ON tbl_allocation.vehicle_id = tbl_vehicles.id
 				INNER JOIN `tbl_vehicle_model` ON tbl_vehicle_model.id = tbl_vehicles.model_id 
 				INNER JOIN `tbl_vehicle_make` ON tbl_vehicle_model.make_id = tbl_vehicle_make.id
-				WHERE   `vehicle_id`= ?  ",$res[0]['vehicle_id']);
+				WHERE   `vehicle_id`= ?  ",$res[$key]['vehicle_id']);
 
-				//pre($res);
-				
-				if(isset($res[0])){
-					// return $res;
-					$mGroup = new Grouptrips();
-					$gtrips = $mGroup->get_grouptrip($this->trips);
-					//pre($gtrips);
-					if(count($gtrips) != 0 && !array_key_exists('error', $gtrips)){
-						foreach ($gtrips as $gtrip_key => $user) {
-							//pre($user);
-							$res[0]['group'][] = $user['email']; 
-						}
+				if (isset($res1[0])) {
+					foreach ($res1 as $key1 => $value) {
+						//$res[0]['driver'][]= $res1[$key];
+						$res[$key]['vehicle_driver']= $res1[$key1];
 					}
-					if (isset($res1[0])) {
-						// foreach ($res1 as $key => $value) {
-						// 	$res[0]['driver'][]= $res1[$key];
-						// }
-						$res[0]['vehicle_driver']= $res1[0];
-					}
-					 
+				} 
 
-					 return $res;
-				}else{
-					return array('error' => 'Trips not found' );
-				}
+				$mGroup = new Grouptrips();
+				$gtrips = $mGroup->get_grouptrip($res[$key]['id']);
+				//pre($gtrips);
+				if(count($gtrips) != 0 && !array_key_exists('error', $gtrips)){
+					foreach ($gtrips as $gtrip_key => $user) {
+						//pre($user);
+						$res[$key]['group'][] = $user['email']; 
+					}
+				}		
+			}
+			return $res;
 
 			// $res = query("SELECT * FROM `tbl_trips` WHERE trip_creator = ?",$trip_creator);
 			// if($res == null){
@@ -234,16 +225,13 @@
 		function start_trip($id,$start_mileage){
 			$res = query("SELECT `id`,`start_mileage`,`end_mileage`,`date`,`vehicle_id`,`start_time`,`stop_time`,`trip_creator`,`start_coordinate`,`end_coordinate`, `approval` FROM `tbl_trips` WHERE `id` = ? AND `approval`= ?",$id, 2);
 			if($res == null){
-				return array('error'=>'trip has not approved');
+				return array('error'=>'Could not update trip');
 			}
 			else{
 				$date = date('Y-m-d H:i:s');
 				$this->trips = $res[0]["id"];
 				$res=query("UPDATE `tbl_trips` SET `start_mileage`=?,`approval`= 5, `start_time`=? WHERE `id`=?",
 					$start_mileage,$date,$id);
-				/*//regenerate token expiry key
-				$token = new Token();
-				$t = $token->generateToken($this->uid,$api_access);*/
 				return $this->getTrip();
 
 			}
@@ -252,7 +240,7 @@
 		function stop_trip($id,$end_mileage,$actual_fare){
 			$res = query("SELECT `id`,`start_mileage`,`end_mileage`,`date`,`vehicle_id`,`start_time`,`stop_time`,`trip_creator`,`start_coordinate`,`end_coordinate`, `approval` FROM `tbl_trips` WHERE `id` = ?",$id);
 			if($res == null){
-				return array('error'=>'trip does not exist');
+				return array('error'=>'Trip not found');
 			}
 			else{
 				$date = date('Y-m-d H:i:s');
@@ -261,9 +249,6 @@
 					$end_mileage,$date,$actual_fare,$id);
 
 				$res = query("UPDATE `tbl_vehicles` SET `vehicle_dispatched` = ? WHERE `id`=?",0,$vehicle_id);
-				/*//regenerate token expiry key
-				$token = new Token();
-				$t = $token->generateToken($this->uid,$api_access);*/
 				return $this->getTrip()
 				;
 
